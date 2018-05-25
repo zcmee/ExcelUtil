62f926c2c2892694e67de44909e6c78a313bd933
adam.bryksy <adam.bryksy@ramsat.pl>
2018-05-25 15:39:27 +0200

    Testy readera wczytującego plik Excel

--- src/main/java/com/github/zcmee/excelutil/dtoes/User.java
+++ src/main/java/com/github/zcmee/excelutil/dtoes/User.java
@@ -48,4 +48,24 @@ public class User {
                 ", age=" + age +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+
+        User user = (User) o;
+
+        if (age != user.age) return false;
+        if (firstName != null ? !firstName.equals(user.firstName) : user.firstName != null) return false;
+        return lastName != null ? lastName.equals(user.lastName) : user.lastName == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstName != null ? firstName.hashCode() : 0;
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + age;
+        return result;
+    }
 }
--- src/test/java/com/github/zcmee/excelutil/generic/reader/UserXlsTest.java
+++ src/test/java/com/github/zcmee/excelutil/generic/reader/UserXlsTest.java
@@ -1,31 +1,66 @@
 package com.github.zcmee.excelutil.generic.reader;
 
 import com.github.zcmee.excelutil.dtoes.User;
-import com.github.zcmee.excelutil.exceptions.ConvertException;
 import com.github.zcmee.excelutil.readers.UserReader;
 import com.github.zcmee.excelutil.utils.ExcelOperations;
 import com.github.zcmee.excelutil.utils.FileOperations;
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.junit.Test;
 
 import java.io.File;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class UserXlsTest {
 
     @Test
-    public void testUserFile() throws IOException, InvalidFormatException {
+    public void testUserFileWithCorrectData() {
         File file = FileOperations.getInstance().getFileFromResource("testfiles/users.xls");
 
         Sheet sheet = ExcelOperations.getInstance().getSheet(file, 0);
         UserReader reader = new UserReader(sheet);
         List<User> users =  reader.generateComplaint();
+        List<User> exceptedUsers = getExceptedListUserForExcelFile();
 
-        assertEquals(2, users.size());
+        assertThat(users, hasItems(new User("Piotr", "Kowalski", 33)));
+        assertEquals(users.size(), exceptedUsers.size());
+        assertThat(users, is(exceptedUsers));
+    }
+
+    @Test
+    public void testUserFileWithWrongData() {
+        File file = FileOperations.getInstance().getFileFromResource("testfiles/users.xls");
+
+        Sheet sheet = ExcelOperations.getInstance().getSheet(file, 0);
+        UserReader reader = new UserReader(sheet);
+        List<User> users =  reader.generateComplaint();
+        List<User> exceptedUsers = getWrongListUserForExcelFile();
+
+        assertThat(users, not(equalTo(exceptedUsers)));
+    }
+
+    private List<User> getExceptedListUserForExcelFile() {
+        User user1 = new User("Piotr", "Kowalski", 33);
+        User user2 = new User("Tomasz", "Terka", 29);
+
+        List<User> exceptedUsers = new ArrayList<>(Arrays.asList(user1, user2));
+        return exceptedUsers;
+    }
+
+    private List<User> getWrongListUserForExcelFile() {
+        User user1 = new User("Piotr", "Kowalski", 33);
+        User user2 = new User("Tomasz", "Terka", 45);
+
+        List<User> exceptedUsers = new ArrayList<>(Arrays.asList(user1, user2));
+        return exceptedUsers;
     }
 
 }
